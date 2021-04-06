17830c94d8ffe56fc31592284d387b0b5638fbad
mgsoe <s1810456012@students.fh-hagenberg.at>
2021-04-06 15:47:53 +0200

    DB Logik done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('impfung_id')->unsigned();
+            $table->foreign('impfung_id')
+                ->references('impfung_id')->on('impfung')
+                ->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
